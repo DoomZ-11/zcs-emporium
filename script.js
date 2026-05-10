354be80b89d81dfcb96c05631c6306a9c37ea02e
DoomZ-11 <onez32011@gmail.com>
2026-05-09 20:16:28 -0700

    Update CDN URL version

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/DoomZ-11/zcs-emporium@896d4ab/";
+const BASE_URL = "https://cdn.jsdelivr.net/gh/DoomZ-11/zcs-emporium@f70a046/";
 
 function loadGames() {
     fetch(BASE_URL + 'game-data.json')
